f91c53bf63812bbece6d456d98fcd571197a196d
1ucky40nc3 <louisnwendler@gmail.com>
2022-05-13 18:52:09 +0200

    added torch loader for MNIST

---
+++ datasets.py
@@ -0,0 +1,71 @@
+from typing import List
+
+from functools import partial
+
+import torch
+import torch.utils.data as data
+
+import torchvision
+import torchvision.transforms as transforms
+
+import ml_collections as mlc
+
+import einops
+
+
+
+def to_jax_img(image: torch.Tensor) -> torch.Tensor:
+    return einops.rearrange(image, "c h w -> h w c")
+
+transforms.ToJax = partial(transforms.Lambda, lambd=to_jax_img)
+
+
+
+def transform(
+    config: mlc.ConfigDict
+) -> transforms.Compose:
+    t = [] # TODO: parse transform from config
+    return transforms.Compose([
+        transforms.ToTensor(),
+        *t,
+        transforms.ToJax(),
+    ])
+
+
+def mnist(
+    train: bool,
+    config: mlc.ConfigDict,
+    root: str = "/tmp",
+    download: bool = True
+) -> data.DataLoader:
+    """Return a MNIST loader.
+
+    Example:
+        >>> dataset_config = mlc.ConfigDict()
+        >>> dataset_config.batch_size = 32
+        >>> dataset_config.num_workers = 0
+        >>> loader = mnist(train=True, config=dataset_config)
+        >>> images, labels = next(iter(loader))
+        >>> print(images.shape)
+        torch.Size([32, 28, 28, 1])
+        >>> print(labels.shape)
+        torch.Size([32])
+        >>> print(images.min(), images.max())
+        tensor(0.) tensor(1.)
+    """
+    dataset = torchvision.datasets.MNIST(
+        root=root, 
+        train=train, 
+        transform=transform(config),
+        download=download)
+    
+    loader = data.DataLoader(
+        dataset=dataset,
+        batch_size=config.batch_size,
+        shuffle=train,
+        num_workers=config.num_workers)
+
+    # TODO: implement prefetching
+    return loader
+
+
