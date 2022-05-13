3f27284e3b59da02e675d97b1e636dab2624c00a
1ucky40nc3 <louisnwendler@gmail.com>
2022-05-13 22:15:16 +0200

    added simple cnn model

---
+++ models.py
@@ -0,0 +1,35 @@
+from functools import partial
+
+import jax
+import jax.numpy as jnp
+
+import flax.linen as nn
+
+import einops
+
+
+class CNN(nn.Module):
+    num_classes: int = 10
+
+    @nn.compact
+    def __call__(self, x: jnp.ndarray) -> jnp.ndarray:
+        conv = partial(
+            nn.Conv, 
+            kernel_size=(3, 3))
+        pool = partial(
+            nn.avg_pool, 
+            window_shape=(2, 2), 
+            strides=(2, 2))
+
+        x = conv(features=32)(x)
+        x = nn.relu(x)
+        x = pool(x)
+        x = conv(features=64)(x)
+        x = nn.relu(x)
+        x = pool(x)
+        x = einops.rearrange(
+            x, "n h w c -> n (h w c)")
+        x = nn.Dense(features=256)(x)
+        x = nn.relu(x)
+        x = nn.Dense(features=self.num_classes)(x)
+        return x
