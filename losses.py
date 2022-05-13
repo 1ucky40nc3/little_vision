e1ef52525a40832772815809f9c3f79c154eeff8
1ucky40nc3 <louisnwendler@gmail.com>
2022-05-13 19:36:08 +0200

    added softmax cross entropy loss

---
+++ losses.py
@@ -0,0 +1,20 @@
+from typing import Any
+
+from functools import partial
+
+import jax
+import jax.numpy as jnp
+
+import flax
+
+import optax
+
+
+def softmax_cross_entropy(
+    logits: jnp.ndarray,
+    labels: jnp.ndarray,
+    num_classes: int
+) -> jnp.ndarray:
+    labels = jax.nn.one_hot(labels, num_classes)
+    loss = optax.softmax_cross_entropy(logits, labels)
+    return loss.mean()
