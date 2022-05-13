ebc237a8be46223a7a18c5123662a6db8ba136fa
1ucky40nc3 <louisnwendler@gmail.com>
2022-05-13 19:43:04 +0200

    added simple optimizer

---
+++ optimizer.py
@@ -0,0 +1,18 @@
+import optax
+
+import ml_collections as mlc
+
+
+def tx(config: mlc.ConfigDict) -> optax.GradientTransformation:
+    gc_tx = optax.clip_by_global_norm(config.gc_norm)
+    tx_fn = getattr(optax, config.tx_name, **config.tx_config)
+    wd_tx = optax.additive_weight_decay(config.wd)
+    lr_tx = optax.scale_by_schedule(config.lr_fn(**config.lr_config))
+
+    return optax.chain(
+        gc_tx,
+        tx_fn,
+        wd_tx,
+        lr_tx,
+        optax.scale(-1)
+    )
