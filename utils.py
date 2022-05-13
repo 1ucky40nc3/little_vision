c966d6e5ba47e2d47623fa1695d53ce366cd82c6
1ucky40nc3 <louisnwendler@gmail.com>
2022-05-13 19:36:18 +0200

    added utils

---
+++ utils.py
@@ -0,0 +1,12 @@
+
+import jax
+import jax.numpy as jnp
+
+import einops
+
+
+def shard(array: jnp.ndarray) -> jnp.array:
+    return einops.rearrange(
+        array, 
+        "(d n) ... -> d n ...", 
+        d=jax.local_device_count())
