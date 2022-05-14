ac976c986ca6becd8a39f5c3f60b1e6702f6f36e
1ucky40nc3 <louisnwendler@gmail.com>
2022-05-14 15:54:44 +0200

    fixes

--- optimizers.py
+++ optimizers.py
@@ -1,3 +1,5 @@
+from functools import partial
+
 import optax
 
 import ml_collections as mlc
@@ -7,11 +9,14 @@ import schedules
 
 def tx(config: mlc.ConfigDict) -> optax.GradientTransformation:
     gc_tx = optax.clip_by_global_norm(config.gc_norm)
-    tx_fn = getattr(optax, config.tx_name, **config.tx_config)
+    tx_fn = getattr(optax, config.tx_name)(
+        **config.tx_config)
     wd_tx = optax.additive_weight_decay(config.wd)
     lr_tx = optax.scale_by_schedule(
-        getattr(schedules, config.schedule_name)(
-            **config.schedule_config))
+        getattr(
+            schedules, 
+            config.schedule_name)(
+        **config.schedule_config))
 
     return optax.chain(
         gc_tx,
