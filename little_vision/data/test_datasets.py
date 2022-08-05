5297f104e79c41a3f234e0103709d4e9cd4763fe
1ucky40nc3 <louisnwendler@gmail.com>
2022-08-05 21:38:54 +0200

    fix tests

--- little_vision/data/test_datasets.py
+++ little_vision/data/test_datasets.py
@@ -1,14 +1,15 @@
-from little_vision.configs import default
+from little_vision import configs
 from little_vision.data import datasets
+from little_vision.data import utils
 
 
 def test_mnist():
-    config = default.get_config()
-    print(config)
+    config = configs.default.get_config()
+
     ds = datasets.mnist(train=True, config=config)
-    it = datasets.prepare(ds, config)
+    it = utils.prepare(ds, config)
+
     x, y = next(iter(it))
-    print(x.shape)
     assert x.shape == (1, 512, 28, 28, 1)
     assert y.shape == (1, 512)
 
@@ -16,8 +17,10 @@ def test_mnist():
 def test_mnist_mlpmixer():
     config = configs.default.get_config()
     config.transform = configs.models.mlpmixer.preprocessing(config)
+
     ds = datasets.mnist(train=True, config=config)
-    it = datasets.prepare(ds, config)
+    it = utils.prepare(ds, config)
+
     x, y = next(iter(it))
     assert x.shape == (1, 512, 28, 28, 1)
     assert y.shape == (1, 512, 10)
@@ -25,11 +28,11 @@ def test_mnist_mlpmixer():
 
 def test_cifar100():
     config = configs.default.get_config(
-        dataset="cifar100"
-    )
+        dataset="cifar100")
+    
     ds = datasets.cifar100(train=True, config=config)
-    it = datasets.prepare(ds, config)
+    it = utils.prepare(ds, config)
+
     x, y = next(iter(it))
-    print(x.shape)
     assert x.shape == (1, 512, 32, 32, 3)
     assert y.shape == (1, 512)
--- little_vision/data/test_utils.py
+++ little_vision/data/test_utils.py
@@ -1,5 +1,21 @@
 import numpy as np
 
+import jax
+
+import torch
+
+from little_vision import configs
+from little_vision.data import utils
+
 
 def test_to_jax_img():
-    img = 
+    img = np.ones((32, 3, 224, 224))
+    img = utils.to_jax_img(img)
+    assert img.shape == (32, 224, 224, 3)
+
+
+def test_shard():
+    d = jax.local_device_count()
+    img = np.ones((32*d, 224, 224, 3))
+    img = utils.shard(img)
+    assert img.shape == (d, 32, 224, 224, 3)
