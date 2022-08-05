adde39afb9d239564c3d783b9d2dc7ae507bee08
1ucky40nc3 <louisnwendler@gmail.com>
2022-08-05 20:55:54 +0200

    change setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,8 @@
 from setuptools import setup
-from setuptools import find_packages
 
 
 setup(
-    name='little_vision',
-    version='0.1.0',
-    packages=find_packages(
-        include=[
-            'little_vision', 
-            'little_vision.*'
-        ],
-    ),
+    name="little_vision",
+    version="0.0.1",
+    packages=["little_vision"],
 )
