c6c102329d466b18c0b6d979542494435342b238
1ucky40nc3 <louisnwendler@gmail.com>
2022-08-05 22:16:34 +0200

    removed imports

--- little_vision/models/__init__.py
+++ little_vision/models/__init__.py
@@ -1,17 +1,3 @@
-from . import cnn
-from .cnn import CNN
-from . import resnet
-from .resnet import ResNet18
-from .resnet import ResNet34
-from . import vit
-from .vit import ViTSmall
-from .vit import ViTBase
-from . import coatnet
-from . import mlpmixer
-from .mlpmixer import MLPMixerS
-from .mlpmixer import MLPMixerB
-from . import layers
-
 """Note:
 
 Parts of this code have been taken from other authors.
