1a96100325cd7232524064cb5d6a96e29e660492
1ucky40nc3 <louisnwendler@gmail.com>
2022-08-07 20:52:57 +0200

    fixed drop path implementation

--- little_vision/models/layers.py
+++ little_vision/models/layers.py
@@ -7,25 +7,30 @@ import flax.linen as nn
 
 
 class DropPath(nn.Module):
+    """Drop path implementation.
+    
+    For reference see:
+    https://github.com/rwightman/efficientnet-jax/blob/a65811fbf63cb90b9ad0724792040ce93b749303/jeffnet/linen/layers/stochastic.py
+    """
     rate: float = 0.
 
     @nn.compact
     def __call__(
         self, 
         x: jnp.ndarray, 
-        deterministic: bool = False
+        deterministic: bool = True,
+        **kwargs
     ) -> jnp.ndarray:
         if deterministic or self.rate == 0.:
             return x
 
         rng = self.make_rng('dropout')
-        print("drop_path", x.shape)
 
         keep_prob = 1. - self.rate
         mask = jax.random.bernoulli(
             key=rng, 
             p=keep_prob, 
-            shape=(x.shape[0], 1, 1))
+            shape=(x.shape[0], 1, 1, 1))
         mask = jnp.broadcast_to(
             mask, x.shape)
         return jax.lax.select(
