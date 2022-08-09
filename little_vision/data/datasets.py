8bef1526d3aaf0604a1462d8d012627f77632553
1ucky40nc3 <louisnwendler@gmail.com>
2022-08-09 21:21:22 +0200

    moved prepare function

--- little_vision/data/datasets.py
+++ little_vision/data/datasets.py
@@ -30,10 +30,56 @@ import ml_collections as mlc
 
 import einops
 
+from timm.data.mixup import Mixup
+
 from little_vision.data.transforms import transform
+from little_vision.data import utils
 
 
 ConfigDict = Union[mlc.ConfigDict, mlc.FrozenConfigDict]
+
+
+def prepare(
+    iterator: Iterator, 
+    config: ConfigDict
+) -> Iterator:
+    is_list = lambda x: isinstance(x, list)
+
+    if config.transform.mixup:
+        mixup = Mixup(
+            **config.transform.mixup_config)
+        mixup_fn = lambda t: mixup(*t)
+
+        iterator = (
+             jax.tree_map(
+                mixup_fn,
+                b,
+                is_leaf=is_list)
+             for b in iterator)
+
+    iterator = (
+        jax.tree_map(
+            lambda t: t.numpy(), b)
+        for b in iterator)
+
+    to_jax = (
+        lambda t: t 
+        if t.ndim != 4 
+        else utils.to_jax_img(t))
+    iterator = (
+        jax.tree_map(
+            to_jax, b) 
+        for b in iterator)
+
+    iterator = (
+        jax.tree_map(
+            utils.shard, b) 
+        for b in iterator)
+    
+    iterator = jax_utils.prefetch_to_device(
+        iterator, config.dataset.prefetch_size)
+
+    return iterator
 
 
 def mnist(
--- little_vision/data/utils.py
+++ little_vision/data/utils.py
@@ -3,7 +3,6 @@ from typing import Iterator
 
 import torch
 
-from timm.data.mixup import Mixup
 
 import jax
 import jax.numpy as jnp
@@ -27,46 +26,3 @@ def shard(array: jnp.ndarray):
       array, 
       "(d n) ... -> d n ...", 
       d=jax.local_device_count())
-
-
-def prepare(
-    iterator: Iterator, 
-    config: ConfigDict
-) -> Iterator:
-    is_list = lambda x: isinstance(x, list)
-
-    if config.transform.mixup:
-        mixup = Mixup(
-            **config.transform.mixup_config)
-        mixup_fn = lambda t: mixup(*t)
-
-        iterator = (
-             jax.tree_map(
-                mixup_fn,
-                b,
-                is_leaf=is_list)
-             for b in iterator)
-
-    iterator = (
-        jax.tree_map(
-            lambda t: t.numpy(), b)
-        for b in iterator)
-
-    to_jax = (
-        lambda t: t 
-        if t.ndim != 4 
-        else to_jax_img(t))
-    iterator = (
-        jax.tree_map(
-            to_jax, b) 
-        for b in iterator)
-
-    iterator = (
-        jax.tree_map(
-            shard, b) 
-        for b in iterator)
-    
-    iterator = jax_utils.prefetch_to_device(
-        iterator, config.dataset.prefetch_size)
-
-    return iterator
