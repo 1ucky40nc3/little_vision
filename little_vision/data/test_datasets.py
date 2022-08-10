cce93653d14efaf113224c71184ec33e314b4ea6
1ucky40nc3 <louisnwendler@gmail.com>
2022-08-10 10:57:31 +0200

    updated test

--- little_vision/data/test_datasets.py
+++ little_vision/data/test_datasets.py
@@ -3,11 +3,16 @@ from little_vision.data import datasets
 from little_vision.data import utils
 
 
+def test_prepare():
+    # TODO: Implement test.
+    pass
+
+
 def test_mnist():
     config = configs.default.get_config()
 
     ds = datasets.mnist(train=True, config=config)
-    it = utils.prepare(ds, config)
+    it = datasets.prepare(ds, config)
 
     x, y = next(iter(it))
     assert x.shape == (1, 512, 28, 28, 1)
@@ -19,7 +24,7 @@ def test_mnist_mlpmixer():
     config.transform = configs.models.mlpmixer.preprocessing(config)
 
     ds = datasets.mnist(train=True, config=config)
-    it = utils.prepare(ds, config)
+    it = datasets.prepare(ds, config)
 
     x, y = next(iter(it))
     assert x.shape == (1, 512, 28, 28, 1)
@@ -31,7 +36,7 @@ def test_cifar100():
         dataset="cifar100")
     
     ds = datasets.cifar100(train=True, config=config)
-    it = utils.prepare(ds, config)
+    it = datasets.prepare(ds, config)
 
     x, y = next(iter(it))
     assert x.shape == (1, 512, 32, 32, 3)
