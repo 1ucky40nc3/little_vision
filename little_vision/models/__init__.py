adbe90de89dd95d3799ed16771981f369d3f2b7a
1ucky40nc3 <louisnwendler@gmail.com>
2022-08-11 14:09:59 +0200

    added alternate implementation for coatnet

--- little_vision/models/__init__.py
+++ little_vision/models/__init__.py
@@ -3,6 +3,7 @@ from .resnet import *
 from .vit import *
 from .mlpmixer import *
 from .coatnet import *
+from .coanetish import *
 
 """Note:
 
--- little_vision/models/coatnetish.py
+++ little_vision/models/coatnetish.py
@@ -1,5 +1,103 @@
+"""
+A major part of this code is translated from https://github.com/microsoft/Swin-Transformer
+"""
+
+import jax.numpy as jnp
+import flax.linen as nn
+
+
+
+from typing import Iterable, Optional
+
+
+class WindowAttention(nn.Module):
+    dim: int
+    num_heads: int
+    window_size: Iterable[int]
+    use_bias: bool = True
+    att_drop: float = 0.0
+    proj_drop: float = 0.0
+    deterministic: Optional[bool] = None
+
+    def get_rel_pos_index(self):
+        h_indices = jnp.arange(0, self.window_size[0])
+        w_indices = jnp.arange(0, self.window_size[1])
+        indices = jnp.stack(jnp.meshgrid(w_indices, h_indices, indexing="ij"))
+        flatten_indices = jnp.reshape(indices, (2, -1))
+        relative_indices = flatten_indices[:, :, None] - flatten_indices[:, None, :]
+        relative_indices = jnp.transpose(relative_indices, (1, 2, 0))
+        relative_indices = relative_indices.at[:, :, 0].add(self.window_size[0] - 1)
+        relative_indices = relative_indices.at[:, :, 1].add(self.window_size[1] - 1)
+        relative_indices = relative_indices.at[:, :, 0].multiply(
+            2 * self.window_size[1] - 1
+        )
+        relative_pos_index = jnp.sum(relative_indices, -1)
+        return relative_pos_index
+
+    @nn.compact
+    def __call__(self, inputs, mask=None, deterministic=None):
+
+        deterministic = nn.merge_param(
+            "deterministic", self.deterministic, deterministic
+        )
+
+        rpbt = self.param(
+            "relative_position_bias_table",
+            nn.initializers.normal(0.02),
+            (
+                (2 * self.window_size[0] - 1) * (2 * self.window_size[1] - 1),
+                self.num_heads,
+            ),
+        )
+
+        relative_pos_index = self.variable(
+            "relative_position_index", "relative_position_index", self.get_rel_pos_index
+        )
+
+        batch, n, channels = inputs.shape
+        qkv = nn.Dense(self.dim * 3, use_bias=self.use_bias, name="qkv")(inputs)
+        qkv = qkv.reshape(batch, n, 3, self.num_heads, channels // self.num_heads)
+        qkv = jnp.transpose(qkv, (2, 0, 3, 1, 4))
+        q, k, v = qkv[0], qkv[1], qkv[2]
+
+        q = q * ((self.dim // self.num_heads) ** -0.5)
+        att = q @ jnp.swapaxes(k, -2, -1)
+
+        rel_pos_bias = jnp.reshape(
+            rpbt[jnp.reshape(relative_pos_index.value, (-1))],
+            (
+                self.window_size[0] * self.window_size[1],
+                self.window_size[0] * self.window_size[1],
+                -1,
+            ),
+        )
+        rel_pos_bias = jnp.transpose(rel_pos_bias, (2, 0, 1))
+        att += jnp.expand_dims(rel_pos_bias, 0)
+
+        if mask is not None:
+            att = jnp.reshape(
+                att, (batch // mask.shape[0], mask.shape[0], self.num_heads, n, n)
+            )
+            att = att + jnp.expand_dims(jnp.expand_dims(mask, 1), 0)
+            att = jnp.reshape(att, (-1, self.num_heads, n, n))
+            att = nn.softmax(att)
+
+        else:
+            att = nn.softmax(att)
+
+        att = nn.Dropout(self.att_drop)(att, deterministic)
+
+        x = jnp.reshape(jnp.swapaxes(att @ v, 1, 2), (batch, n, channels))
+        x = nn.Dense(self.dim, name="proj")(x)
+        x = nn.Dropout(self.proj_drop)(x, deterministic)
+        return x
+
+
 from typing import Any
+from typing import Tuple
 from typing import Union
+from typing import Optional
+from typing import Callable
 
 from functools import partial
 
@@ -14,20 +112,136 @@ import einops
 
 from little_vision.models import resnet
 from little_vision.models import vit
+from little_vision.models import layers
 
 
 DType = Any
 Module = Union[partial, nn.Module]
 
+"""CoAtNet implementation inspired by:
+https://github.com/blakechi/ComVEX/tree/master/comvex/coatnet
+"""
 
-class Identity(nn.Module):
+
+class ConvStem(nn.Module):
+    features: int = 64
+    kernel_size: Tuple[int] = (3, 3)
+    strides: Optional[int] = None
+    
+    @nn.compact
+    def __call__(
+        self, 
+        x: jnp.ndarray,
+        **kwargs
+    ) -> jnp.ndarray:
+        conv = partial(
+            nn.Conv,
+            features=self.features,
+            kernel_size=self.kernel_size,
+            padding=1)
+
+        x = conv(strides=2)(x)
+        x = conv(strides=1)(x)
+
+        return x
+
+
+def global_avg_pool(
+    x: jnp.ndarray
+) -> jnp.ndarray:
+    return jnp.mean(
+        x, axis=(1, 2), keepdims=True)
+
+
+class SqueezeExcitationBlock(nn.Module):
+    se_rate: int = 0.25
+    pool: Callable = global_avg_pool
+    conv: Module = partial(
+        nn.Conv,
+        kernel_size=(1, 1),
+        strides=1,
+        use_bias=True)
+
     @nn.compact
     def __call__(
         self,
         x: jnp.ndarray,
         **kwargs
     ) -> jnp.ndarray:
-        return x
+        reduced = int(x.shape[-1] * self.se_rate)
+        
+        y = self.pool(x)
+        y = self.conv(
+            features=reduced)(y)
+        y = nn.relu(y)
+        y = self.conv(
+            features=x.shape[-1])(y)
+        y = nn.sigmoid(y)
+
+        return x * y 
+
+
+class MBConvBlock(nn.Module):
+    features: int = 64
+    exp_rate: int = 4
+    kernel_size: Tuple[int] = (3, 3)
+    strides: int = 1
+    se_rate: float = 0.25
+    norm: Module = nn.BatchNorm
+    act: Callable = nn.swish
+    drop_path_rate: float = 0.
+
+    @nn.compact
+    def __call__(
+        self,
+        x: jnp.ndarray,
+        deterministic: bool = False,
+        **kwargs
+    ) -> jnp.ndarray:
+        expansion = x.shape[-1] * self.exp_rate
+        conv = partial(
+            nn.Conv,
+            kernel_size=(1, 1),
+            use_bias=False)
+        norm = partial(
+            self.norm,
+            use_running_average=deterministic)
+        
+        y = norm()(x)
+        y = nn.swish(y)
+        y = conv(
+            features=expansion,
+            strides=self.strides,
+            name="expansion")(y)
+
+        y = norm()(y)
+        y = nn.swish(y)
+        y = nn.Conv(
+            features=expansion,
+            kernel_size=(3, 3),
+            strides=1,
+            padding=1,
+            feature_group_count=expansion,
+            use_bias=False,
+            name="depthwise")(y)
+
+        if self.se_rate > 0.:
+            y = SqueezeExcitationBlock(
+                se_rate=self.se_rate)(y)
+
+        y = norm()(y)
+        y = conv(
+            features=self.features,
+            strides=1,
+            name="projection")(y)
+
+        if self.features == x.shape[-1] and self.strides == 1:
+            y = layers.DropPath(
+                rate=self.drop_path_rate
+            )(y, deterministic=deterministic)
+            y += x
+
+        return y
 
 
 class TransformerFFN(nn.Module):
@@ -43,6 +257,7 @@ class TransformerFFN(nn.Module):
         **kwargs
     ) -> jnp.ndarray:
         dim = x.shape[-1]
+
         x = self.ffm(
             features=dim * 4)(x)
         x = self.act(x)
@@ -54,11 +269,10 @@ class TransformerFFN(nn.Module):
 
 class TransformerBlock(nn.Module):
     features: int = 64
-    strides: int = 2
-
+    strides: int = 1
     norm: Module = nn.LayerNorm
     attn: Module = partial(
-        nn.SelfAttention,
+        WindowAttention,
         num_heads=32)
     pool: Module = partial(
         nn.max_pool,
@@ -69,96 +283,85 @@ class TransformerBlock(nn.Module):
         kernel_size=(1, 1)
     )
     ffn: Module = TransformerFFN
+    drop_path_rate: float = 0.1
 
     @nn.compact
     def __call__(
         self,
         x: jnp.ndarray,
+        deterministic: bool = False,
         **kwargs
     ) -> jnp.ndarray:
-        h = x.shape[1]
-        y = x
+        h = x.shape[1] // 2 if self.strides == 2 else x.shape[1]
 
+        attn = partial(
+            self.attn,
+            dim=self.features,
+            window_size=(h, h)
+        )
 
-        y = self.norm()(y)
-        y = self.pool()(y) if self.strides == 2 else y
+        if self.strides > 1:
+            x = nn.Conv(
+                features=self.features,
+                kernel_size=(7, 7),
+                strides=self.strides,
+            )(x)
+            x = nn.LayerNorm()(x)
+        
         y = einops.rearrange(
-            y, "n h w c -> n (h w) c")
-        y = self.attn(
-            out_features=self.features)(y)
+            x, "n h w c -> n (h w) c")
+        y = attn()(y, deterministic=deterministic)
         y = einops.rearrange(
             y, "n (h w) c -> n h w c", h=h)
-
-        x = self.pool()(x) if self.strides == 2 else x
-        x = self.proj(
-            features=self.features)(x)
         
         x += y
 
-        x = self.ffn()(x)
+        y = self.ffn()(x)
+        y = layers.DropPath(
+            rate=self.drop_path_rate
+        )(y, deterministic=deterministic)
 
-        return x
+        return x + y
 
 
 class CoAtNetish(nn.Module):
     num_classes: int = 10
+    layers: Tuple[int] = (2, 2, 3, 5, 2)
+    features: Tuple[int] = (64, 96, 192, 384, 768)
+    layout: Tuple[str] = ("S", "C", "C", "T", "T")
+    stem: Module = ConvStem
+    conv: Module = MBConvBlock
+    trans: Module = TransformerBlock
     dtype: DType = jnp.float32
 
-    norm: Module = partial(
-        nn.BatchNorm,
-        momentum=0.9)
+    def get_layer(
+        self,
+        name: str
+    ) -> Module:
+        return {
+            "S": self.stem,
+            "C": self.conv,
+            "T": self.trans
+        }[name]
 
     @nn.compact
     def __call__(
         self,
         x: jnp.ndarray,
-        deterministic: bool = True,
+        deterministic: bool = False,
         **kwargs
     ) -> jnp.ndarray:
-        norm = partial(
-            self.norm,
-            use_running_average=deterministic,
-            dtype=self.dtype)
+        assert len(self.layers) == len(self.features) == len(self.layout) == 5
 
-        conv_block = partial(
-            resnet.BottleneckResNetBlock,
-            norm=norm
-        )
-
-        # s0
-        for i in range(2):
-            strides = (2, 2) if i == 0 else (1, 1)
-            x = conv_block(
-                features=64,
-                strides=strides)(x)
-        
-        # s1
-        for i in range(2):
-            strides = (2, 2) if i == 0 else (1, 1)
-            x = conv_block(
-                features=96,
-                strides=strides)(x)
-
-        # s2
-        for i in range(3):
-            strides = (2, 2) if i == 0 else (1, 1)
-            x = conv_block(
-                features=192,
-                strides=strides)(x)
-
-        # s3
-        for i in range(5):
-            strides = (2, 2) if i == 0 else (1, 1)
-            x = TransformerBlock(
-                features=384,
-                strides=strides)(x)
-
-        # s4
-        for i in range(2):
-            strides = (2, 2) if i == 0 else (1, 1)
-            x = TransformerBlock(
-                features=768,
-                strides=strides)(x)
+        for i, (n, f, l) in enumerate(
+            zip(self.layers, self.features, self.layout)):
+            for j in range(n):
+                strides = (2, 2) if j == 0 else (1, 1)
+                x = self.get_layer(l)(
+                    features=f,
+                    strides=strides,
+                    name=f"{l}{i}-{j}"
+                )(x, deterministic=deterministic)
 
         x = einops.reduce(
             x, "n h w c -> n c", "mean")
@@ -169,13 +372,13 @@ class CoAtNetish(nn.Module):
         return x
 
 
-if __name__ == "__main__":
-    num_classes = 1000
-    dims = (224, 224, 3)
-    rng = jax.random.PRNGKey(42)
+CoAtNetish0 = CoAtNet
 
-    model = CoAtNetish(
-        num_classes=num_classes)
-    variables = model.init(rng, jnp.ones([1, *dims]))
-    num = sum(p.size for p in jax.tree_leaves(variables["params"]))
-    print(num)
+CoAtNetish1 = partial(
+    CoAtNet,
+    layers=(2, 2, 6, 14, 2))
+
+CoAtNetish2 = partial(
+    CoAtNet,
+    layers=(2, 2, 6, 14, 2),
+    features=(128, 128, 256, 512, 1024))
